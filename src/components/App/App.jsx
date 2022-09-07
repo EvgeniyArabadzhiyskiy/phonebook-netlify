b1db03024b994ba7f8e8159c73adc483140eaff6
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-09-07 19:33:17 +0300

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,43 +1,18 @@
-// import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-import SharedLayout from 'components/SharedLayout/SharedLayout';
-// import ContactPage from 'pages/ContactPage/ContactPage';
-import HomePage from 'pages/HomePage/HomePage';
-import { Route, Routes } from 'react-router-dom';
-
-// import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import userOperations from 'redux/auth/auth-operations';
-// import authSelectors from 'redux/auth/auth-selectors';
-// import PublicRoute from 'components/Routes/PublicRoute';
-import About from 'pages/About/About';
-
-// import { useAuth } from '../../hooks';
+// import SharedLayout from 'components/SharedLayout/SharedLayout';
+// import HomePage from 'pages/HomePage/HomePage';
+// import { Route, Routes } from 'react-router-dom';
+// import About from 'pages/About/About';
 
 const App = () => {
-  // const dispatch = useDispatch();
-
-  // const { isFetchCurrentUser } = useAuth()
-  // const isFetchCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
-
-  // useEffect(() => {
-  //   dispatch(userOperations.fetchCurrentUser());
-  // }, [dispatch]);
-
   return (
     <>
-      <>
-        <Routes>
+      <h1>hello</h1>
+      {/* <Routes>
           <Route path="/" element={<SharedLayout />}>
             <Route index element={<HomePage />} />
             <Route path="about" element={<About />} />
-            {/* <Route path="contacts" element={<ContactPage />} /> */}
           </Route>
-        </Routes>
-      </>
-
-      {/* <ToastContainer autoClose={3000} theme="colored" pauseOnHover /> */}
+        </Routes> */}
     </>
   );
 };
