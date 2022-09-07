096a1716c2a4b29aa1c3753c11308648c768d5d8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-09-07 19:44:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
